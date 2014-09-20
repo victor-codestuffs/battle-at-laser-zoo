ab5dadfbdb27f627b1e0fd106cb92030578d6ac2
Victor Hoang <vic.hoang@yahoo.com>
2014-09-20 11:55:03 -0700

    Merge refactor

--- js/game.js
+++ js/game.js
@@ -8,16 +8,10 @@ function preload() {
   game.load.image('creep', 'assets/creep.png');
 }
 
-// TODO: Refactor code for 2 animals
-
-var animal;
-var animal2;
-var creeps;
-var key = {};
-var key2 = {};
-var bullets, bulletTime = 0;
-var fireRate = 100;
-var nextFire = 0;
+var hero1, hero2
+var key1 = {}, key2 = {};
+var creeps, bullets, bulletTime = 0;
+var fireRate = 100, nextFire = 0;
 
 var p1special = 0;
 var p2special = 0;
@@ -30,22 +24,25 @@ function create() {
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  animal = game.add.sprite(50, 50, 'bear');
-  animal.anchor.setTo(0.5, 0.5);
-  animal.scale.setTo(0.75, 0.75);
-  animal.animations.add('bear_run', [1, 2, 3, 4, 5, 6, 7], 8, true);
-  animal.animations.add('bear_chomp', [9, 10, 11, 12, 13, 14, 15], 8, true);
-  animal.animations.add('bear_idle', [0], 10, true);
+  // Configure keyboard
+  key1 = {
+    up: _addKey(Phaser.Keyboard.W),
+    down: _addKey(Phaser.Keyboard.S),
+    left: _addKey(Phaser.Keyboard.A),
+    right: _addKey(Phaser.Keyboard.D),
+    basic: _addKey(Phaser.Keyboard.SPACEBAR)
+  };
+  key2 = {
+    up: _addKey(Phaser.Keyboard.UP),
+    down: _addKey(Phaser.Keyboard.DOWN),
+    left: _addKey(Phaser.Keyboard.LEFT),
+    right: _addKey(Phaser.Keyboard.RIGHT),
+    basic: _addKey(Phaser.Keyboard.ENTER)
+  }
 
-  animal2 = game.add.sprite(550, 550, 'chicken');
-  animal2.anchor.setTo(0.5, 0.5);
-  animal2.rotation = 180;
-
-  game.physics.enable(animal, Phaser.Physics.ARCADE);
-  game.physics.enable(animal2, Phaser.Physics.ARCADE);
-
-  animal.body.collideWorldBounds = true;
-  animal2.body.collideWorldBounds = true;
+  // Make dem heroes
+  hero1 = new Hero('bear', key1);
+  hero2 = new Hero('chicken', key2);
 
   bullets = game.add.group();
   bullets.enableBody = true;
@@ -56,127 +53,27 @@ function create() {
   bullets.setAll('checkWorldBounds', true);
   bullets.setAll('outOfBoundsKill', true);
 
-  key.up = game.input.keyboard.addKey(Phaser.Keyboard.W);
-  key.down = game.input.keyboard.addKey(Phaser.Keyboard.S);
-  key.left = game.input.keyboard.addKey(Phaser.Keyboard.A);
-  key.right = game.input.keyboard.addKey(Phaser.Keyboard.D);
-  key.space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
-  key2.up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-  key2.down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-  key2.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-  key2.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-  key2.enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-  
-  key.space.onDown.add(chomp, this);
-  key2.enter.onDown.add(chomp2, this);
-
   // add creeps
   creeps = game.add.group();
-
-  for (var i = 0; i < 30; i++)
-  {
-    var creep = creeps.create(game.rnd.integerInRange(100, 700), game.rnd.integerInRange(32, 700), 'creep');
-    // creep.animations.add('spin', [0,1,2,3]);
-    // creep.play('spin', 20, true);
-    game.physics.enable(creep, Phaser.Physics.ARCADE);
-    creep.body.velocity.x = game.rnd.integerInRange(-100, 100);
-    creep.body.velocity.y = game.rnd.integerInRange(-100, 100);
-  }
-
-  creeps.setAll('body.collideWorldBounds', true);
-  creeps.setAll('body.bounce.x', 1);
-  creeps.setAll('body.bounce.y', 1);
-  creeps.setAll('body.minBounceVelocity', 0);
-
-  game.time.events.repeat(Phaser.Timer.SECOND * 10, 20, resurrect, this);
+  _createCreeps(20);
+  game.time.events.repeat(Phaser.Timer.SECOND * 10, 20, _resurrectCreep, this);
 }
 
 function update() {
+  hero1.update();
+  hero2.update();
 
-  animal.body.velocity.x = 0;
-  animal.body.velocity.y = 0;
-  animal.body.angularVelocity = 0;
+  // fast chomp
+  // if (key.space.isDown) { chomp(); }
 
-  // calculate movement in terms of character angle. changes direction with left and right.
-
-  if (key.left.isDown) {
-    animal.body.angularVelocity = -200;
-  } else if (key.right.isDown) {
-    animal.body.angularVelocity = 200;
-  }
-
-  if (key.up.isDown) {
-    animal.animations.play('bear_run');
-    game.physics.arcade.velocityFromAngle(animal.angle + 90, 300, animal.body.velocity);
-  } else if (key.down.isDown) {
-    animal.animations.play('bear_run');
-    game.physics.arcade.velocityFromAngle(animal.angle + 90, -200, animal.body.velocity);
-  } 
-
-  // animal 2
-  animal2.body.velocity.x = 0;
-  animal2.body.velocity.y = 0;
-  animal2.body.angularVelocity = 0;
-
-  if (key2.left.isDown) {
-    animal2.body.angularVelocity = -200;
-  } else if (key2.right.isDown) {
-    animal2.body.angularVelocity = 200;
-  }
-
-  if (key2.up.isDown) {
-    game.physics.arcade.velocityFromAngle(animal2.angle + 90, 300, animal2.body.velocity);
-  } else if (key2.down.isDown) {
-    game.physics.arcade.velocityFromAngle(animal2.angle + 90, -200, animal2.body.velocity);
-  }
-
-  if (key.space.isDown) {
-    animal.animations.play('bear_chomp');
-  }
-  // if (key.space.isDown) {
-  //   chomp();
-  // }
-
-  game.physics.arcade.collide(animal, animal2);
-  game.physics.arcade.collide(creeps, animal2);
-  game.physics.arcade.collide(creeps, animal);
-  game.physics.arcade.collide(creeps, bullets, fireballCreepCallback, null, this);
-}
-
-// basic attack
-function chomp() {
-  if (game.time.now > bulletTime) {
-    //  Grab the first bullet we can from the pool
-    bullet = bullets.getFirstExists(false);
-    if (bullet) {
-      bullet.reset(animal.body.x + 16, animal.body.y + 16);
-      bullet.lifespan = 150;
-      bullet.rotation = animal.angle;
-      bullet.player = 1;
-      game.physics.arcade.velocityFromAngle(animal.angle + 90, 800, bullet.body.velocity);
-      bulletTime = game.time.now + 50;
-    }
-  }
-}
-
-function chomp2() {
-  if (game.time.now > bulletTime) {
-    //  Grab the first bullet we can from the pool
-    bullet = bullets.getFirstExists(false);
-    if (bullet) {
-      bullet.reset(animal2.body.x + 16, animal2.body.y + 16);
-      bullet.lifespan = 150;
-      bullet.rotation = animal2.angle;
-      bullet.player = 2;
-      game.physics.arcade.velocityFromAngle(animal2.angle + 90, 800, bullet.body.velocity);
-      bulletTime = game.time.now + 50;
-    }
-  }
+  game.physics.arcade.collide(hero1, hero2);
+  game.physics.arcade.collide(creeps, hero2);
+  game.physics.arcade.collide(creeps, hero1);
+  game.physics.arcade.collide(creeps, bullets, _fireballCreepCallback, null, this);
 }
 
 // creep die collision
-function fireballCreepCallback (_creeps, _bullets) {
+function _fireballCreepCallback (_creeps, _bullets) {
   _creeps.kill();
   _bullets.kill();
   if (_bullets.player == 1) {
@@ -190,21 +87,142 @@ function fireballCreepCallback (_creeps, _bullets) {
   }
 }
 
-function resurrect() {
+function _createCreeps(num) {
+  for (var i = 0; i < num; i++) {
+    var creep = creeps.create(_rnd(100, 700), _rnd(32, 700), 'creep');
+    // creep.animations.add('spin', [0,1,2,3]);
+    // creep.play('spin', 20, true);
+    game.physics.enable(creep, Phaser.Physics.ARCADE);
+    creep.body.velocity.x = _rnd(-100, 100);
+    creep.body.velocity.y = _rnd(-100, 100);
+  }
 
-  //  Get a dead item
-  var creep = creeps.getFirstDead();
+  creeps.setAll('body.collideWorldBounds', true);
+  creeps.setAll('body.bounce.x', 1);
+  creeps.setAll('body.bounce.y', 1);
+  creeps.setAll('body.minBounceVelocity', 0);
+}
+
+function _resurrectCreep() {
+  var creep = creeps.getFirstDead(); //  Get a dead item
 
   if (creep) {
-    //  And bring it back to life
-    creep.reset(game.world.randomX, game.world.randomY);
-
-    //  This just changes its frame
-    creep.frame = game.rnd.integerInRange(0, 36);
-
-    creep.body.velocity.x = game.rnd.integerInRange(-100, 100);
-    creep.body.velocity.y = game.rnd.integerInRange(-100, 100);
+    creep.reset(game.world.randomX, game.world.randomY); //  And bring it back to life
+    // creep.frame = _rnd(0, 36); 
+    creep.body.velocity.x = _rnd(-100, 100);
+    creep.body.velocity.y = _rnd(-100, 100);
   }
+}
+
+// key bind helper
+function _addKey(key) {
+  return game.input.keyboard.addKey(key);
+}
+
+// random number helper
+function _rnd(low, high) {
+  return game.rnd.integerInRange(low, high);
+}
+
+// HERO MAKER
+function Hero(type, key) {
+  var sprite;
+
+  switch (type) {
+    // START BEAR
+    case 'bear':
+      sprite = game.add.sprite(50, 50, 'bear')
+      sprite.anchor.setTo(0.5, 0.5);
+      sprite.scale.setTo(0.75, 0.75);
+      sprite.animations.add('bear_run', [1, 2, 3, 4, 5, 6, 7], 8, true);
+      sprite.animations.add('bear_chomp', [9, 10, 11, 12, 13, 14, 15], 8, true);
+      sprite.animations.add('bear_idle', [0], 10, true);
+      sprite.chomp = function () {
+        if (game.time.now > bulletTime) {
+          //  Grab the first bullet we can from the pool
+          bullet = bullets.getFirstExists(false);
+          if (bullet) {
+            bullet.reset(sprite.body.x + 16, sprite.body.y + 16);
+            bullet.lifespan = 150;
+            bullet.rotation = sprite.angle;
+            bullet.player = 1;
+            game.physics.arcade.velocityFromAngle(sprite.angle + 90, 800, bullet.body.velocity);
+            bulletTime = game.time.now + 50;
+          }
+        }
+      };
+      sprite.update = function () {
+        sprite.body.velocity.x = 0;
+        sprite.body.velocity.y = 0;
+        sprite.body.angularVelocity = 0;
+
+        if (key.left.isDown) {
+          sprite.body.angularVelocity = -200;
+        } else if (key.right.isDown) {
+          sprite.body.angularVelocity = 200;
+        }
+
+        if (key.up.isDown) {
+          sprite.animations.play('bear_run');
+          game.physics.arcade.velocityFromAngle(sprite.angle + 90, 300, sprite.body.velocity);
+        } else if (key.down.isDown) {
+          sprite.animations.play('bear_run');
+          game.physics.arcade.velocityFromAngle(sprite.angle + 90, -200, sprite.body.velocity);
+        }
+
+        if (key.basic.isDown) {
+          sprite.animations.play('bear_chomp');
+        }
+
+        key.basic.onDown.add(sprite.chomp, this);
+      }
+      break;
+    // END BEAR
+    // START DEFAULT (CHICKEN)
+    default:
+      sprite = game.add.sprite(550, 550, 'chicken');
+      sprite.anchor.setTo(0.5, 0.5);
+      sprite.rotation = 180;
+      sprite.chomp = function () {
+        if (game.time.now > bulletTime) {
+          //  Grab the first bullet we can from the pool
+          bullet = bullets.getFirstExists(false);
+          if (bullet) {
+            bullet.reset(sprite.body.x + 16, sprite.body.y + 16);
+            bullet.lifespan = 150;
+            bullet.rotation = sprite.angle;
+            bullet.player = 2;
+            game.physics.arcade.velocityFromAngle(sprite.angle + 90, 800, bullet.body.velocity);
+            bulletTime = game.time.now + 50;
+          }
+        }
+      };
+      sprite.update = function () {
+        sprite.body.velocity.x = 0;
+        sprite.body.velocity.y = 0;
+        sprite.body.angularVelocity = 0;
+
+        if (key.left.isDown) {
+          sprite.body.angularVelocity = -200;
+        } else if (key.right.isDown) {
+          sprite.body.angularVelocity = 200;
+        }
+
+        if (key.up.isDown) {
+          game.physics.arcade.velocityFromAngle(sprite.angle + 90, 300, sprite.body.velocity);
+        } else if (key.down.isDown) {
+          game.physics.arcade.velocityFromAngle(sprite.angle + 90, -200, sprite.body.velocity);
+        }
+
+        key.basic.onDown.add(sprite.chomp, this);
+      }
+    // END DEFAULT
+  }
+  game.physics.enable(sprite, Phaser.Physics.ARCADE);
+  sprite.body.collideWorldBounds = true;
+
+  return sprite;
+
 }
 
 function updateBars() {
@@ -221,8 +239,3 @@ function updateBars() {
     $('.barContainer.p2').removeClass('glow');
   }
 }
-
-$( document ).ready(function() {
-  $('body').append('<div class="barContainer p1"><div class="barFill"></div></div>');
-  $('body').append('<div class="barContainer p2"><div class="barFill p2"></div></div>');
-});
