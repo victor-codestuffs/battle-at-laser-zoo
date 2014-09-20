196d4b1dfdc8b6e4e733703ee81cd62aa27a4651
Victor Hoang <vic.hoang@yahoo.com>
2014-09-19 23:01:52 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  var port = 1337;
+
+  require( 'load-grunt-tasks' )( grunt );
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: { port: port }
+      },
+      start: {
+        options: {
+          port: port,
+          open: 'http://localhost:<%= connect.server.options.port %>/'
+        },
+        app: 'Google Chrome'
+      }
+    },
+    watch: {
+      options: { livereload: true },
+      js: {
+        files: ['js/game.js'],
+        options: { spawn: false }
+      },
+      html: {
+        files: ['index.html'],
+        options: { spawn: false }
+      }
+    }
+  });
+
+  grunt.registerTask('start', ['connect:start', 'watch']);
+  grunt.registerTask('default', ['connect:server','watch']);
+  
+};
---
+++ js/game.js
@@ -0,0 +1,91 @@
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'phaser-stage', { preload: preload, create: create, update: update });
+
+function preload() {
+  game.load.image('blue-line', 'assets/blue-line.png');
+  game.load.image('red-line', 'assets/red-line.png');
+  game.load.image('orange-line', 'assets/orange-line.png');
+  game.load.image('dark-line', 'assets/dark-line.png');
+  game.load.image('ship', 'assets/ship.png');
+}
+
+var ship;
+var cursors;
+var upKey, downKey, leftKey, rightKey, aKey, wKey, sKey, dKey;
+
+function create() {
+
+  game.stage.backgroundColor = '#010e1e';
+
+  //  In this example we'll create 4 specific keys (up, down, left, right) and monitor them in our update function
+
+  upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+  downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+  leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+  rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+  aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+  wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+  sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+  dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+
+  var em1 = game.add.emitter(game.world.width, game.world.centerY, 200);
+
+  // emitter.width = game.world.width;
+  em1.height = game.world.height / 3;
+  // em1.angle = 30; // uncomment to set an angle
+  em1.makeParticles(['blue-line', 'red-line', 'orange-line', 'dark-line']);
+  em1.gravity = 0;
+  em1.minParticleScale = 0.1;
+  em1.maxParticleScale = 1;
+  em1.setYSpeed(-2, 2);
+  em1.setXSpeed(-1000, -500);
+  em1.minRotation = 0;
+  em1.maxRotation = 0;
+  em1.start(false, 4000, 20, 0);
+
+  var em2 = game.add.emitter(game.world.width, game.world.centerY, 100);
+
+  em2.height = game.world.height;
+  em2.makeParticles('dark-line');
+  em2.gravity = 0;
+  em2.minParticleScale = 0.5;
+  em2.maxParticleScale = 1;
+  em2.setYSpeed(-2, 2);
+  em2.setXSpeed(-500, -250);
+  em2.minRotation = 0;
+  em2.maxRotation = 0;
+  em2.start(false, 4000, 20, 0);
+
+    //  Add a sprite
+  ship = game.add.sprite(50, game.world.centerY - 50, 'ship');
+  ship2 = game.add.sprite(150, game.world.centerY - 50, 'ship');
+
+}
+
+function update() {
+
+  if (upKey.isDown) {
+    ship.y--;
+  } else if (downKey.isDown) {
+    ship.y++;
+  }
+
+  if (leftKey.isDown) {
+    ship.x--;
+  } else if (rightKey.isDown) {
+    ship.x++;
+  }
+
+  if (wKey.isDown) {
+    ship2.y--;
+  } else if (sKey.isDown) {
+    ship2.y++;
+  }
+
+  if (aKey.isDown) {
+    ship2.x--;
+  } else if (dKey.isDown) {
+    ship2.x++;
+  }
+
+}
